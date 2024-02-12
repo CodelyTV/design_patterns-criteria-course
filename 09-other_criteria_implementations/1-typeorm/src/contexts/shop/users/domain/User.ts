c9d2f4f940522d191970777d98a826686722ed1f
Rafa Gómez <rgomezcasas@gmail.com>
2024-02-12 12:52:50 +0100

    feat(9-1): add base

---
+++ 09-other_criteria_implementations/1-typeorm/src/contexts/shop/users/domain/User.ts
@@ -0,0 +1,47 @@
+import { UserEmail } from "./UserEmail";
+import { UserId } from "./UserId";
+import { UserName } from "./UserName";
+import { UserProfilePicture } from "./UserProfilePicture";
+
+export type UserPrimitives = {
+	id: string;
+	name: string;
+	email: string;
+	profilePicture: string;
+};
+
+export class User {
+	private constructor(
+		public readonly id: UserId,
+		private readonly name: UserName,
+		private readonly email: UserEmail,
+		private readonly profilePicture: UserProfilePicture,
+	) {}
+
+	static create(id: string, name: string, email: string, profilePicture: string): User {
+		return new User(
+			new UserId(id),
+			new UserName(name),
+			new UserEmail(email),
+			new UserProfilePicture(profilePicture),
+		);
+	}
+
+	static fromPrimitives(primitives: UserPrimitives): User {
+		return new User(
+			new UserId(primitives.id),
+			new UserName(primitives.name),
+			new UserEmail(primitives.email),
+			new UserProfilePicture(primitives.profilePicture),
+		);
+	}
+
+	toPrimitives(): UserPrimitives {
+		return {
+			id: this.id.value,
+			name: this.name.value,
+			email: this.email.value,
+			profilePicture: this.profilePicture.value,
+		};
+	}
+}
