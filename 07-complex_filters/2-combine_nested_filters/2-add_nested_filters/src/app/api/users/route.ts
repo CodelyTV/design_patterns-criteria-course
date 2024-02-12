23d054016d48ee19b5cb78245ef5f83b82a61eb1
Rafa Gómez <rgomezcasas@gmail.com>
2024-02-12 12:16:40 +0100

    feat(7-2): add example

--- 07-complex_filters/2-combine_nested_filters/2-add_nested_filters/src/app/api/users/route.ts
+++ 07-complex_filters/2-combine_nested_filters/2-add_nested_filters/src/app/api/users/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
 	const { searchParams } = new URL(request.url);
 
 	const users = await searcher.search(
-		searchParams.get("filters"),
+		searchParams.get("filters") as string,
 		searchParams.get("orderBy"),
 		searchParams.get("order"),
 		searchParams.has("pageSize") ? parseInt(searchParams.get("pageSize") as string, 10) : null,
