9ba0e518409ab73797cec261702a4802087ac807
Rafa Gómez <rgomezcasas@gmail.com>
2024-06-10 23:18:32 +0200

    feat(10-3): add example

--- 10-improvements/3-integrate_ecosystem_criteria/routes/web.php
+++ 10-improvements/3-integrate_ecosystem_criteria/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Requests\SearchUserRequest;
+use App\Models\User;
+use ComplexHeart\Domain\Criteria\Criteria;
+use ComplexHeart\Infrastructure\Laravel\Persistence\EloquentCriteriaParser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('users', function (SearchUserRequest $request) {
+    $criteria = Criteria::fromSource($request);
+
+    $builder = User::query();
+
+    $parser = new EloquentCriteriaParser();
+
+     return $parser
+        ->applyCriteria($builder, $criteria)
+        ->get();
 });
